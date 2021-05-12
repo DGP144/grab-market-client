2d16e0530ada4bc5a5c71febd1f7872e26f66ef6
DGP <daegeun2821@gmail.com>
2021-05-12 21:50:59 +0900

    products.detail

--- src/App.js
+++ src/App.js
@@ -7,17 +7,25 @@ import UploadPage from "./upload";
 function App() {
   return (
     <div>
-      <switch>
-        <Route exact={true} path={"/"}>
-          <MainPageComponent />
-        </Route>
-        <Route exact={true} path={"/products/:id"}>
-          <ProductPage />
-        </Route>
-        <Route exact={true} path={"/upload"}>
-          <UploadPage />
-        </Route>
-      </switch>
+      <div id="header">
+        <div id="header-area">
+          <img src="/images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body">
+        <switch>
+          <Route exact={true} path={"/"}>
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path={"/products/:id"}>
+            <ProductPage />
+          </Route>
+          <Route exact={true} path={"/upload"}>
+            <UploadPage />
+          </Route>
+        </switch>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
